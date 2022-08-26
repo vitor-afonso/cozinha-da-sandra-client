965c4bbe832c5d41cdc71687dbbba4eb0255de3b
Vitor Afonso <vittokm@hotmail.com>
2022-08-26 18:19:24 +0100

    added search to docesPage and salgadosPage

--- src/pages/DocesPage.js
+++ src/pages/DocesPage.js
@@ -1,17 +1,28 @@
 // jshint esversion:9
 
-import { Box, CircularProgress, Grid, Typography } from '@mui/material';
-import { useEffect } from 'react';
+import { Box, CircularProgress, Grid, InputAdornment, TextField, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { ShopItem } from '../components/ShopItem/ShopItemCard';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 const DocesPage = () => {
   const { shopItems, isLoading } = useSelector((store) => store.items);
+  const [str, setStr] = useState('');
+  const [filteredItems, setFilteredItems] = useState([]);
 
   const docesClasses = {
     container: {
       px: 3,
       mb: 12,
+    },
+    field: {
+      minWidth: '300px',
+      maxWidth: '600px',
+      marginTop: 0,
+      marginBottom: 5,
+      mx: 'auto',
+      display: 'block',
     },
   };
 
@@ -19,17 +30,42 @@ const DocesPage = () => {
     window.scrollTo(0, 0);
   }, []);
 
+  useEffect(() => {
+    if (str === '' && shopItems.length > 0) {
+      setFilteredItems(shopItems);
+    } else {
+      let filteredItems = shopItems.filter((item) => item.name.toLowerCase().includes(str.toLowerCase()));
+      setFilteredItems(filteredItems);
+    }
+  }, [str, shopItems]);
+
   return (
     <Box sx={docesClasses.container}>
       <Typography variant='h2' color='primary' sx={{ my: '25px' }}>
         DOCES
       </Typography>
 
+      <TextField
+        label='Procurar'
+        type='text'
+        variant='outlined'
+        fullWidth
+        sx={docesClasses.field}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position='start'>
+              <SearchOutlinedIcon />
+            </InputAdornment>
+          ),
+        }}
+        onChange={(e) => setStr(e.target.value)}
+      />
+
       {isLoading && <CircularProgress sx={{ mt: 20 }} />}
 
-      {shopItems.length !== 0 && (
+      {filteredItems.length !== 0 && (
         <Grid container spacing={2}>
-          {shopItems.map((element) => {
+          {filteredItems.map((element) => {
             if (element.category === 'doces') {
               return (
                 <Grid item key={element._id} xs={12} sm={6} md={4} lg={3}>
@@ -39,6 +75,11 @@ const DocesPage = () => {
             }
           })}
         </Grid>
+      )}
+      {filteredItems.length === 0 && (
+        <Typography paragraph color='neutral' sx={{ my: '25px' }}>
+          Nenhum item encontrado.
+        </Typography>
       )}
     </Box>
   );
--- src/pages/SalgadosPage.js
+++ src/pages/SalgadosPage.js
@@ -1,23 +1,43 @@
 // jshint esversion:9
 
-import { Box, CircularProgress, Grid, Typography } from '@mui/material';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-
 import { ShopItem } from '../components/ShopItem/ShopItemCard';
+
+import { Box, CircularProgress, Grid, InputAdornment, TextField, Typography } from '@mui/material';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 const SalgadosPage = () => {
   const { shopItems, isLoading } = useSelector((store) => store.items);
+  const [str, setStr] = useState('');
+  const [filteredItems, setFilteredItems] = useState([]);
 
   const salgadosClasses = {
     container: {
       px: 3,
       mb: 12,
     },
+    field: {
+      minWidth: '300px',
+      maxWidth: '600px',
+      marginTop: 0,
+      marginBottom: 5,
+      mx: 'auto',
+      display: 'block',
+    },
   };
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
+  useEffect(() => {
+    if (str === '' && shopItems.length > 0) {
+      setFilteredItems(shopItems);
+    } else {
+      let filteredItems = shopItems.filter((item) => item.name.toLowerCase().includes(str.toLowerCase()));
+      setFilteredItems(filteredItems);
+    }
+  }, [str, shopItems]);
 
   return (
     <Box sx={salgadosClasses.container}>
@@ -25,11 +45,27 @@ const SalgadosPage = () => {
         SALGADOS
       </Typography>
 
+      <TextField
+        label='Procurar'
+        type='text'
+        variant='outlined'
+        fullWidth
+        sx={salgadosClasses.field}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position='start'>
+              <SearchOutlinedIcon />
+            </InputAdornment>
+          ),
+        }}
+        onChange={(e) => setStr(e.target.value)}
+      />
+
       {isLoading && <CircularProgress sx={{ mt: 20 }} />}
 
-      {shopItems.length !== 0 && (
+      {filteredItems.length !== 0 && (
         <Grid container spacing={2}>
-          {shopItems.map((element) => {
+          {filteredItems.map((element) => {
             if (element.category === 'salgados') {
               return (
                 <Grid item key={element._id} xs={12} sm={6} md={4} lg={3}>
@@ -39,6 +75,11 @@ const SalgadosPage = () => {
             }
           })}
         </Grid>
+      )}
+      {filteredItems.length === 0 && (
+        <Typography paragraph color='neutral' sx={{ my: '25px' }}>
+          Nenhum item encontrado.
+        </Typography>
       )}
     </Box>
   );
