8afbf99ddd7f6daafe295a7f781da0ee09c5b904
Vitor Afonso <vittokm@hotmail.com>
2023-04-26 21:24:37 +0100

    Update hook to handle reviews data

--- src/hooks/useHomePageItems.js
+++ src/hooks/useHomePageItems.js
@@ -3,9 +3,19 @@ import { useSelector } from 'react-redux';
 import { APP, getHomePageCategoryItems } from '../utils/app.utils';
 
 export default function useHomePageItems() {
+  const { shopOrders } = useSelector((store) => store.orders);
   const { shopItems } = useSelector((store) => store.items);
   const [docesData, setDocesData] = useState(null);
   const [salgadosData, setSalgadosData] = useState(null);
+  const [reviewsData, setReviewsData] = useState(null);
+
+  useEffect(() => {
+    if (shopOrders.length > 0) {
+      const allReviews = shopOrders.filter((order) => order.reviewId).map((order) => order.reviewId);
+      const ratingTotal = allReviews.reduce((accumulator, review) => accumulator + review.rating, 0);
+      setReviewsData({ average: Math.round(ratingTotal / allReviews.length / 0.5) * 0.5, reviews: allReviews });
+    }
+  }, [shopOrders]);
 
   useEffect(() => {
     if (shopItems.length > 0) {
@@ -14,11 +24,11 @@ export default function useHomePageItems() {
       setDocesData({ categoryItems: filteredDoces, categoryName: APP.categories.doces });
       setSalgadosData({ categoryItems: filteredSalgados, categoryName: APP.categories.salgados });
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [shopItems]);
 
   return {
     docesData,
     salgadosData,
+    reviewsData,
   };
 }
