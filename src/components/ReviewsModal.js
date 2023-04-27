47afb8f9ef84127258d035ce1473b68dbacf6768
Vitor Afonso <vittokm@hotmail.com>
2023-04-27 19:49:40 +0100

    Create reviews modal

---
+++ src/components/ReviewsModal.js
@@ -0,0 +1,44 @@
+import { Box, Modal, Rating, Typography } from '@mui/material';
+import React from 'react';
+import { componentProps, reviewsModalStyle } from '../utils/app.styleClasses';
+import { parseDateToShow } from '../utils/app.utils';
+
+const ReviewsModal = ({ isModalOpen, setIsModalOpen, reviewsData }) => {
+  return (
+    <>
+      {reviewsData && (
+        <Modal open={isModalOpen} onClose={() => setIsModalOpen(false)}>
+          <Box sx={reviewsModalStyle}>
+            {reviewsData.reviews.map((review) => {
+              return (
+                <Box sx={{ width: '250px', mb: 8 }} key={review._id}>
+                  <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: 32 }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                      <Typography variant={componentProps.variant.h6} sx={{ fontSize: 18 }}>
+                        {reviewsData.averageRating}
+                      </Typography>
+                      <Rating name='read-only' defaultValue={review.rating} size='small' />
+                    </Box>
+
+                    <Typography variant={componentProps.variant.h6} sx={{ fontSize: 14 }} color={componentProps.color.textSecondary}>
+                      {review.author}
+                    </Typography>
+                  </Box>
+                  <Typography variant={componentProps.variant.h6} sx={{ fontSize: 14 }} color={componentProps.color.textSecondary}>
+                    {parseDateToShow(review.createdAt)}
+                  </Typography>
+
+                  <Typography variant={componentProps.variant.h6}>{review.title}</Typography>
+
+                  <Typography variant='body2'>{review.content}</Typography>
+                </Box>
+              );
+            })}
+          </Box>
+        </Modal>
+      )}
+    </>
+  );
+};
+
+export default ReviewsModal;
