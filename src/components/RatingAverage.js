cdfba6f0ec23cd9c88de52fd03d8ae8de8d7f0de
Vitor Afonso <vittokm@hotmail.com>
2023-04-28 20:53:31 +0100

    Update ratingAverage text to be lowercase

--- src/components/RatingAverage.js
+++ src/components/RatingAverage.js
@@ -10,7 +10,9 @@ const RatingAverage = ({ setIsModalOpen }) => {
       <Button sx={reviewsClasses.reviewsBtn} onClick={() => setIsModalOpen(true)}>
         <Typography variant={componentProps.variant.h5}>{averageRating}</Typography>
         <Rating name='rating-average' defaultValue={averageRating} max={1} readOnly />
-        <Typography variant={componentProps.variant.body2}>({` ${numberOfReviews} reviews`})</Typography>
+        <Typography variant={componentProps.variant.body1} sx={{ textTransform: 'lowercase', fontSize: 16 }}>
+          ({` ${numberOfReviews} reviews`})
+        </Typography>
       </Button>
     </Box>
   );
